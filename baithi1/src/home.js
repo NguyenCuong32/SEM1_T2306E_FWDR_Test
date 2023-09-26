8dbb61fd7834fa374142d5808f15f6977a81740d
Viet2111 <nguyenducviet211197@gmail.com>
2023-09-26 19:42:10 +0700

    nộp bài

---
+++ baithi1/src/home.js
@@ -0,0 +1,11 @@
+import React from "react";
+function Home(){
+    return(
+        <div>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSmlJGeDFoDO2mUm5q3S8O_oc-8O4BYFWjNemRIdQ_6LQ&s" style={{ width: '500px', height: 'auto' }} />
+        <h4>ACB limited liability company</h4>
+        <p>specializing inproviding...</p> 
+        </div>
+    )
+}
+export default Home;
---
+++ baithi1/src/register.js
@@ -0,0 +1,24 @@
+import './register.css'
+function Register(){
+    return(
+        <div className="register1">
+            <div>
+            <h4>Product</h4>
+            <h4>Name</h4>
+            <h4>Phone Number</h4>
+            <h4>Email</h4>
+            </div>
+            <div className="register2">
+                <input></input>
+                <br></br>
+                <input></input>
+                <br></br>
+                <input></input>
+                <br></br>
+                <input></input>
+                <br></br>            
+            </div>
+        </div>
+    )   
+}
+export default Register;
