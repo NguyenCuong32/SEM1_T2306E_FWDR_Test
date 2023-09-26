3997277c579a50f71befed4b407240c7cf16bcf8
nguyenanhvu <ttnav0710@gmail.com>
2023-09-26 19:27:02 +0700

    test

--- mytest/src/pages/Home.js
+++ mytest/src/pages/Home.js
@@ -1,7 +1,7 @@
 function Home () {
   return (
     <div>
-    <img src="https://images.unsplash.com/photo-1631631480669-535cc43f2327?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1287&q=80" alt=""
+    <img src="https://th.bing.com/th/id/OIP.Ime4OtKJsZUh49g4On7FKAHaKs?pid=ImgDet&rs=1" alt=""
     />
     <h2>ABC limited liability company</h2>
     <h3>Specializing in providing information technology solution in the fields of AI, Finance, ...</h3>
