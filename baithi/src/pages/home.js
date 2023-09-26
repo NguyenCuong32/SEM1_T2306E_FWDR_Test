0fcc053c370cd73c5b15582f21875f36df3619b0
ducdv1976 <141699605+ducdv1976@users.noreply.github.com>
2023-09-26 19:24:04 +0700

    ok

--- baithi/src/pages/home.js
+++ baithi/src/pages/home.js
@@ -1,4 +1,4 @@
 const Home = () => {
-    return <p><a href="https://www.pinterest.com/3ChicksPhoto/actorsmodels/">Người mẫu-diễn viên nỗi tiếng</a></p>
+    return <p><img src="https://vi.wikipedia.org/wiki/T%E1%BA%ADp_tin:M%C3%BCnster,_LVM,_B%C3%BCrogeb%C3%A4ude_--_2013_--_5149-51.jpg/">introduces the company </a></p>
 };
 export default Home;
