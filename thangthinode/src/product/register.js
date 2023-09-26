b6ac560f42a921b664a94889f6021e798d57fce7
1Thang <khanhhuyen050709@gmail.com>
2023-09-26 19:19:52 +0700

    tiep

--- thangthinode/src/product/register.js
+++ thangthinode/src/product/register.js
@@ -1,7 +1,36 @@
-
+import React from "react";
+import "./rigister.css"
 function Register(){
-    return(
-   <></>
+    return (
+        <div style={{margin:"60px"}}>
+            <form>
+            <div>
+            <table>
+                <tr>
+                    <th>product</th>
+                    <td><input ></input></td>
+                </tr>
+                <tr>
+                    <th>Name</th>
+                    <td><input ></input></td>
+                </tr>
+                <tr>
+                    <th>Phone Number</th>
+                    <td><input ></input></td>
+                </tr>
+                <tr>
+                    <th>Email</th>
+                    <td><input  ></input></td>
+                </tr>
+            </table>
+               
+            </div>
+            <div style={{margin:"20px 0 0 150px", }}>
+                <buton className="Register">Register</buton>
+                <buton className="cancel" >Cancel</buton>
+            </div>
+            </form>
+        </div>
     )
-    }
-    export default Register;
+}
+export default Register
